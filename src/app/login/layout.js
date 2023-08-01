690b1cc8527182b90fdd034c0ce525ee755ecfe0
272085 <tushar.chandra@ics-global.in>
2023-08-01 17:42:17 +0530

    Conditional Layout using Pathname hook

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,23 +1,31 @@
+"use client"
 import Link from "next/link"
 import './login.css'
+import { usePathname } from "next/navigation"
 
 export default function Layout({ children }) {
+    const pathName = usePathname()
     return (
         <div>
-            <ul className="menu-login">
-                <li>
-                    <h4>Login Menu</h4>
-                </li>
-                <li>
-                    <Link href="/login">Login Main</Link>
-                </li>
-                <li>
-                    <Link href="/login/login-teacher">Login Teacher</Link>
-                </li>
-                <li>
-                    <Link href="/login/login-student">Login Student</Link>
-                </li>
-            </ul>
+            {
+                pathName !== "/login/login-teacher"
+                    ?
+                    <ul className="menu-login">
+                        <li>
+                            <h4>Login Menu</h4>
+                        </li>
+                        <li>
+                            <Link href="/login">Login Main</Link>
+                        </li>
+                        <li>
+                            <Link href="/login/login-teacher">Login Teacher</Link>
+                        </li>
+                        <li>
+                            <Link href="/login/login-student">Login Student</Link>
+                        </li>
+                    </ul>
+                    : ""
+            }
             {children}
         </div>
     )
