2ebb98e627e2f536a5165e7ea86d86513c05e8a8
Tushar Chandra <tusharchandra10@gmail.com>
2023-08-08 15:37:37 +0530

    Redirection using redirect method in next/navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,35 @@
 "use client"
 import Link from 'next/link'
 import { useRouter } from "next/navigation"
+import "./homepage.css"
 
 export default function Home() {
   const router = useRouter()
   const navigate = (name) => {
     router.push(name)
   }
+  const pathArr = [
+    "about",
+    "carts",
+    "login",
+    "product",
+    "quote",
+    "student-list",
+    "study",
+    "users"
+  ]
+  
   return (
     <main>
       <h1>Home Page</h1>
-      <Link href="/login">Login Page</Link>
-      <br />
-      <Link href="/about">About Page</Link>
-      <br />
-      <Link href="/product">Product Page</Link>
-      <br />
+      <div className='homepage-navigation'>
+        <ol>
+          {pathArr.map(path => {
+            const url = "/" + path
+            return <li><Link href={url}>{path} Page</Link></li>
+          })}
+        </ol>
+      </div>
       <button onClick={() => navigate("/login")}>Go to Login</button>
       <button onClick={() => navigate("/about")}>Go to About</button>
     </main>
