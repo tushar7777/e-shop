1d262f875911bf0df5680c9d6b98cc475fc4dc0e
Tushar Chandra <tusharchandra10@gmail.com>
2023-08-08 16:08:06 +0530

    Redirection using NextJs Config for static and dynamic URL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,22 @@ const nextConfig = {
     images: {
         domains: ["www.vectorlogo.zone"]
     },
-    output: "export"
+    // output: "export",
+    redirects: async () => {
+        return [
+            {
+                source: "/redirect-page-2",
+                destination: "/carts",
+                permanent: false
+            },
+            {
+                //dynamic page
+                source: "/redirect-page-2/:redirectPageId",
+                destination: "/carts/10",
+                permanent: false
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
